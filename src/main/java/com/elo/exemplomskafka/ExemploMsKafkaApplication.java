fef3f1e3993f2ed188cfd3705e40c00180761c40
Leandro Gustavo Neis <lgn200@gmail.com>
2024-06-17 16:17:59 -0300

    Criando o projeto e enviando

---
+++ src/main/java/com/elo/exemplomskafka/ExemploMsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.elo.exemplomskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMsKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elo/exemplomskafka/ExemploMsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elo.exemplomskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMsKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
